2d3b6e710c7f78c3ce2f7e0e0ee5bb4cdb1aa725
BokFang <1092554430@qq.com>
2019-08-04 16:18:18 +0800

    增加清屏功能和画笔粗细选择

--- canvas.js
+++ canvas.js
@@ -3,6 +3,7 @@ var context = board.getContext("2d")
 autoCanvasSet(board)
 userListening(board)
 model(board)
+var lineWidth = 5
 function drawCircle(x,y,R){
   context.beginPath()
   context.arc(x, y, R, 0, 2 * Math.PI)
@@ -11,12 +12,12 @@ function drawCircle(x,y,R){
 function drawLine(x1,y1,x2,y2){
   context.beginPath()
   context.moveTo(x1, y1)
-  context.lineWidth = 5
+  context.lineWidth = lineWidth
   context.lineTo(x2, y2)
   context.stroke()
   context.closePath()
 }
-var using = false;
+var using = false
 var lastPoint = {x:undefined,y:undefined}
 function userListening(board){
 //特性检测
@@ -25,98 +26,97 @@ if(document.body.ontouchstart !== undefined){
   //按下屏幕
   document.body.style.overflow='hidden';  
   board.ontouchstart = function(a){
-  var x = a.touches[0].clientX;
-  var y = a.touches[0].clientY;
-  using = true;
+  var x = a.touches[0].clientX
+  var y = a.touches[0].clientY
+  using = true
   if(eraserEnabled){
     context.clearRect(x-5,y-5,10,10);
   }else{
-    drawCircle(x,y,1);
+    drawCircle(x,y,1)
     lastPoint = {"x":x,"y":y}
   }
 }
 //移动手指
 board.ontouchmove = function(a){
-  var x = a.touches[0].clientX;
-  var y = a.touches[0].clientY;
+  var x = a.touches[0].clientX
+  var y = a.touches[0].clientY
   if(!using){ return }
   if(eraserEnabled){
-    context.clearRect(x-5,y-5,10,10);
+    context.clearRect(x-5,y-5,10,10)
   }else{
-    using = true;
+    using = true
     var newPoint = {"x":x,"y":y}
-    drawLine(lastPoint.x,lastPoint.y,newPoint.x,newPoint.y);
-    lastPoint = newPoint;
+    drawLine(lastPoint.x,lastPoint.y,newPoint.x,newPoint.y)
+    lastPoint = newPoint
   }
 }
 //松开手指
 board.ontouchend = function(a){
-  using = false;
+  using = false
 }
 }else{
 //非触屏设备
 //按下鼠标
 board.onmousedown = function(a){
-  var x = a.clientX;
-  var y = a.clientY;
-  using = true;
+  var x = a.clientX
+  var y = a.clientY
+  using = true
   if(eraserEnabled){
-    context.clearRect(x-5,y-5,10,10);
+    context.clearRect(x-5,y-5,10,10)
   }else{
-    drawCircle(x,y,1);
+    drawCircle(x,y,1)
     lastPoint = {"x":x,"y":y}
   }
 }
 //动鼠标
 board.onmousemove = function(a){
-  var x = a.clientX;
-  var y = a.clientY;
+  var x = a.clientX
+  var y = a.clientY
   if(!using){ return }
   if(eraserEnabled){
-    context.clearRect(x-5,y-5,10,10);
+    context.clearRect(x-5,y-5,10,10)
   }else{
-    using = true;
+    using = true
     var newPoint = {"x":x,"y":y}
-    drawLine(lastPoint.x,lastPoint.y,newPoint.x,newPoint.y);
-    lastPoint = newPoint;
+    drawLine(lastPoint.x,lastPoint.y,newPoint.x,newPoint.y)
+    lastPoint = newPoint
   }
 }
 //松开鼠标
 board.onmouseup = function(a){
-  using = false;
+  using = false
 }
 }
 }
-var eraser = document.getElementById('eraser');
-var eraserEnabled = false;
+var eraser = document.getElementById('eraser')
+var eraserEnabled = false
 eraser.onclick = function(){
   eraserEnabled = !eraserEnabled;
 }
 function autoCanvasSet(board){
-  fullScreen();
+  fullScreen()
 window.onresize = function(){
-  fullScreen();
+  fullScreen()
 }
 function fullScreen(){
-  var pageWidth = document.documentElement.clientWidth;
-  var pageHeight = document.documentElement.clientHeight;
-  board.width = pageWidth;
-  board.height = pageHeight;
+  var pageWidth = document.documentElement.clientWidth
+  var pageHeight = document.documentElement.clientHeight
+  board.width = pageWidth
+  board.height = pageHeight
 }
 }
 //工具选择
-var pen = document.getElementById('pen');
-var actions = document.getElementById('actions');
+var pen = document.getElementById('pen')
+var actions = document.getElementById('actions')
 function model(board){
   window.onload = function(){
     eraser.onclick = function(){
-      eraserEnabled = true;
+      eraserEnabled = true
       eraser.classList.add('active')
       pen.classList.remove('active')
-      
     }
     pen.onclick = function(){
-      eraserEnabled = false;
+      eraserEnabled = false
       pen.classList.add('active')
       eraser.classList.remove('active')
     }
@@ -155,6 +155,13 @@ blue.onclick = function(){
   green.classList.remove('active')
   blue.classList.add('active')
 }
-
-
+thin.onclick = function(){
+  lineWidth = 5
+}
+thick.onclick = function(){
+  lineWidth = 8
+}
+clear.onclick = function(){
+  context.clearRect(0, 0, board.width, board.height)
+}
 
